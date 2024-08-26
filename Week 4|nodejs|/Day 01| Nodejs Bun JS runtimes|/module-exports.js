2c009bd255fb52e5ffd84be44c6bca2f3a4e6588
zafar1162014 <zafarulhaq1162014@gmail.com>
2024-08-26 21:18:28 +0500

    delete day 1 week4

--- Week 4|nodejs|/Day 01| Nodejs Bun JS runtimes|/module-exports.js
+++
@@ -1,7 +0,0 @@
-// const math = require('./math(Exports-in-file)'); better to use import
-import * as math from './ math(Exports -in -file).js';
-
-console.log(math.sum(5, 4));
-console.log(math.mul(5, 4));
-console.log(math.PI);
-console.log(math.g);
